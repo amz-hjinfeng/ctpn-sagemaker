576844b7762922a3fcf4a5097cee1ffd2c5ae07e
hjinfeng <hjinfeng@amazon.com>
2019-12-03 18:41:53 +0800

    remove the old data

--- ctpn/load-data.sh
+++ ctpn/load-data.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+rm -rf /opt/ml/code/ctpn/data*
+
 mkdir -p /opt/ml/code/ctpn/data/pretrain_model
 curl "https://cigna-demo-aws.s3-ap-southeast-1.amazonaws.com/VGG_imagenet.npy" -o "/opt/ml/code/data/pretrain_model/VGG_imagenet.npy"
 curl "https://cigna-demo-aws.s3-ap-southeast-1.amazonaws.com/VOCdevkit.zip" -o "/opt/ml/input/data/VOCdevkit.zip"
 unzip -n /opt/ml/input/data/VOCdevkit.zip -d /opt/ml/input/data/dat
 mkdir -p /opt/ml/input/data/VOCdevkit2007
+rm -rf /opt/ml/input/data/VOCdevkit.zip
 cp -r /opt/ml/input/data/dat/VOCdevkit/* /opt/ml/input/data/VOCdevkit2007
 ls /opt/ml/input/data/VOCdevkit2007
 rm -rf /opt/ml/input/data/dat
